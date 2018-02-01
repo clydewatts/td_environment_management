4486e6e96bc781cd5d244d62f23ed4f8331715ed
Watts, Clyde <clyde.watts@teradata.com>
2018-02-01 08:01:22 +0000

    xx

--- jenkins/test1.py
+++ jenkins/test1.py
@@ -3,4 +3,5 @@ import teradata
 fh = open("C:/Users/CW171001.TD/PycharmProjects/td_environment_management/jenkins/junk.txt","w")
 print("hello",file=fh)
 fh.close()
+udaexec = teradata.UdaExec(appName="test1",version=1)
 exit(0)
