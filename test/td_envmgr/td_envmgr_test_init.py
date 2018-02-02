f92d199144d83ac41a7df6da306d717d512e4bc4
Watts, Clyde <clyde.watts@teradata.com>
2018-02-02 16:46:32 +0000

    xx

--- test/td_envmgr/td_envmgr_test_init.py
+++ test/td_envmgr/td_envmgr_test_init.py
@@ -8,8 +8,5 @@ import os
 
 # this will test python and jenkins
 import teradata
-fh = open("C:/Users/CW171001.TD/PycharmProjects/td_environment_management/jenkins/junk.txt","w")
-print("hello",file=fh)
-fh.close()
 udaexec = teradata.UdaExec(appName="test1",version=1)
 exit(0)
