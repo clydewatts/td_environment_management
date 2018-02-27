82853f091ff7dc25de2000214728024b9c8c7816
Watts, Clyde <clyde.watts@teradata.com>
2018-02-27 07:13:20 +0000

    xxx

--- test/td_envmgr/td_envmgr_test_init.py
+++ test/td_envmgr/td_envmgr_test_init.py
@@ -9,4 +9,5 @@ import os
 # this will test python and jenkins
 import teradata
 udaexec = teradata.UdaExec(appName="test1",version=1)
+session = udaexec.connect(method='ODBC', system="192.168.31.142", username='dbc', password='dbc')
 exit(0)
